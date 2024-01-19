7feac9eae043947c3435df7133f6d116558c20b3
mikey <ccitzxy@outlook.com>
2024-01-19 11:43:55 +0800

    update

---
+++ src/renderer/src/components/Toast.vue
@@ -0,0 +1,49 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: ''
+  }
+});
+const emits = defineEmits(['close']);
+</script>
+
+<template>
+  <div class="toast">
+    <div class="title">
+      {{ title }}
+      <img class="close" src="../assets/icon-close-circle.svg" @click="emits('close')" />
+    </div>
+    <div class="body">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="less">
+.toast {
+  position: fixed;
+  right: 16px;
+  top: 16px;
+  z-index: 999;
+  padding: 12px 40px 12px 16px;
+  background: #ff8e0d;
+  border-radius: 8px;
+  .title {
+    font-size: 16px;
+    font-weight: 500;
+    color: #fff;
+    .close {
+      cursor: pointer;
+      position: absolute;
+      top: 8px;
+      right: 8px;
+    }
+  }
+  .body{
+    font-size: 14px;
+    margin-top: 4px;
+    color: #efefef;
+  }
+}
+</style>
--- src/renderer/src/views/POW/Monitor/Index.vue
+++ src/renderer/src/views/POW/Monitor/Index.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import Header from '../../../components/Header.vue';
-import { getEthereumToUSD, getIERC20OrderList } from '../../../api/pow';
+import { getEthereumToUSD, getIERC20OrderList, getStatisticsByTick } from '../../../api/pow';
 import Table from '../Main/components/Table.vue';
 import icon_refresh from '../../../assets/icon-refresh.svg';
 import icon_notification from '../../../assets/icon-notification.svg';
 import { formatNumber } from '../../../util';
+import Toast from '../../../components/Toast.vue';
 
 const unitPrice = ref(0);
 const ticks = ['ensc', 'ethi', 'ierc-m4', 'ierc', 'Sparkle Inscription'];
@@ -40,6 +41,7 @@ const loading = ref(false);
 const refresh = async () => {
   loading.value = true;
   clearTimeout(timer);
+
   await getEthereumUnitPrice();
   await getOrderInfo();
   timer = setTimeout(() => {
@@ -47,11 +49,69 @@ const refresh = async () => {
   }, 5000);
   loading.value = false;
 };
+const TICKS = 'TICKS';
+const NEW_TICK = 'NEW_TICK';
+const newTickData = ref(null);
+const showToast = ref(false);
+const tickNumber = ref(0);
+const checkNewTick = () => {
+  const newTick = localStorage.getItem(NEW_TICK);
+  if (newTick) {
+    newTickData.value = JSON.parse(newTick);
+    showToast.value = true;
+  }
+  getStatisticsByTick({
+    limit: 10000,
+    offset: 0
+  }).then((res) => {
+    const { data: ticks, code } = res;
+    const storeTicks = localStorage.getItem(TICKS);
+    if (code === 1) {
+      tickNumber.value = ticks.list.length;
+      if (storeTicks) {
+        const storeTicksList = JSON.parse(storeTicks);
+        if (storeTicksList.length === ticks.list.length) return;
+        ticks.list.map((inscription) => {
+          const find = storeTicksList.find((item) => item.tick === inscription.tick);
+          if (!find) {
+            addNotify({
+              title: '新的铭文：' + inscription.tick,
+              data: 'https://www.ierc20.com/market/' + inscription.tick,
+              body: `创建时间：${new Date().toLocaleString()}`
+            });
+            storeNewTickAndToastIt({
+              tick: inscription.tick,
+              url: 'https://www.ierc20.com/market/' + inscription.tick
+            });
+            showToast.value = true;
+          }
+        });
+      } else {
+        localStorage.setItem(TICKS, JSON.stringify(ticks.list));
+      }
+    }
+    console.log(res.data);
+  });
+};
+const storeNewTickAndToastIt = (data) => {
+  const { tick, url } = data;
+  newTickData.value = {
+    tick: tick,
+    createTime: new Date().toLocaleString(),
+    url: url
+  };
+  localStorage.setItem(NEW_TICK, JSON.stringify(newTickData.value));
+  showToast.value = true;
+};
+const closeToast = () => {
+  localStorage.removeItem(NEW_TICK);
+  showToast.value = false;
+};
 onMounted(async () => {
   navigator.serviceWorker.register('sw.js');
   await refresh();
+  checkNewTick();
 });
-const audio = new Audio('/329.wav');
 const checkOrder = (orders) => {
   const [firstOrder, secondOrder] = orders;
   const unitPrice1 = firstOrder.value / firstOrder.amt;
@@ -65,7 +125,14 @@ function notifyMe(order) {
   if (!('Notification' in window)) {
     return;
   }
-  addNotify(order);
+  addNotify({
+    title: `${order.tick} 新订单！`,
+    data: 'https://www.ierc20.com/market/' + order.tick,
+    body: `数量：${order.amt}，单价：$${formatPrice(
+      order.amt / order.value,
+      4
+    )}，总价：$${formatPrice(order.value, 2)}`
+  });
 
   // 如果使用者還是不同意授權執行 Notification
   // 最好還是進行適當的處理以避免繼續打擾使用者
@@ -73,19 +140,17 @@ function notifyMe(order) {
 const formatPrice = (value, dem = 6) => {
   return formatNumber(value * unitPrice.value, dem);
 };
-const addNotify = (order) => {
+const addNotify = (info) => {
+  const { title, data, body } = info;
   Notification.requestPermission().then((permission) => {
     if (permission === 'granted') {
       navigator.serviceWorker.ready.then(function (registration) {
-        registration.showNotification(`${order.tick} 新订单！`, {
+        registration.showNotification(title, {
           icon: 'icon.png',
           vibrate: [200, 100, 200],
           silent: false,
-          data: 'https://www.ierc20.com/market/' + order.tick,
-          body: `数量：${order.amt}，单价：$${formatPrice(
-            order.amt / order.value,
-            4
-          )}，总价：$${formatPrice(order.value, 2)}`
+          data: data,
+          body: body
         });
       });
     }
@@ -113,12 +178,21 @@ const addNotify = (order) => {
         <div style="flex: 1; color: #ff5733; font-size: 15px">
           <span style="font-weight: bold; font-size: 18px">1</span>
           ETH = <span style="font-weight: bold; font-size: 18px">{{ unitPrice }}</span> USD
+          <span style="padding-left: 30px;">铭文总数：{{ tickNumber }}</span>
         </div>
-        <div class="flex notify" @click="addNotify({ tick: 'ierc', amt: '400', value: '8' })">
+
+        <div
+          class="flex notify"
+          @click="addNotify({ title: '测试通知', body: '测试内容', data: '测试data' })"
+        >
           <img :src="icon_notification" alt="" />
         </div>
       </div>
       <Table :data="tableData" :unit-price="unitPrice" />
+      <Toast v-if="showToast" :title="'新铭文' + newTickData.tick" @close="closeToast">
+        创建时间：{{ newTickData.createTime }}
+        <a target="_blank" :href="newTickData.url">跳转市场</a>
+      </Toast>
     </div>
   </main>
 </template>
